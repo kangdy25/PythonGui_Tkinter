41735ef0d93d293a9847d1a780c9a7bd7a4d6060
DongYoon <canfrog@naver.com>
2021-10-25 11:42:05 +0900

    First pile in PythonGui

---
+++ 1_Create_Frame.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+root.title("Kangdy GUI")
+root.geometry("640x480") #가로 * 세로
+# root.geometry("640x480+100+300") #가로 * 세로 + x좌표 + y좌표
+
+# root.resizable(False, False) # 너비 높이 값 변경 불가
+
+root.mainloop()
