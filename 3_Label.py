2ca03773f6f35fc16a7173a613cc63eeecf859b6
Kangdy <canfrog@naver.com>
2021-10-26 12:16:32 +0900

    modify image path

--- 3_Label.py
+++ 3_Label.py
@@ -9,7 +9,7 @@ root.geometry("640x480") #가로 * 세로
 label1 = Label(root, text="안녕하세요")
 label1.pack()
 
-photo = PhotoImage(file="PythonGui/img/CheckBox.png")
+photo = PhotoImage(file="img/CheckBox.png")
 label2 = Label(root, image=photo)
 label2.pack()
 
@@ -17,7 +17,7 @@ def change():
     label1.config(text="또 만나요")
 
     global photo2
-    photo2 = PhotoImage(file="PythonGui/img/RedX.png")
+    photo2 = PhotoImage(file="img/RedX.png")
     label2.config(image=photo2)
 btn = Button(root, text="클릭", command=change)
 btn.pack()
