9a9be041c155070d5960255a5c6d7898141af6d2
Kangdy <canfrog@naver.com>
2021-10-31 22:57:47 +0900

    Edit Top Menu and Add textbox

--- PythonGui_Memonote/Gui_Memonote.py
+++ PythonGui_Memonote/Gui_Memonote.py
@@ -8,7 +8,17 @@ topmenu = Menu(root)
 
 # 파일 메뉴
 menu_file = Menu(topmenu, tearoff=0)
-menu_file.add_command(label="   새로 만들기")
+menu_file.add_command(label="   새로 만들기(N)")
+menu_file.add_command(label="   새 창(W)")
+menu_file.add_command(label="   열기(O)")
+menu_file.add_command(label="   저장(S)")
+menu_file.add_command(label="   다른 이름으로 저장(A)")
+menu_file.add_separator()
+menu_file.add_command(label="   페이지 설정(U)")
+menu_file.add_command(label="   인쇄(P)")
+menu_file.add_separator()
+menu_file.add_command(label="   끝내기(X)", command=root.quit)
+
 topmenu.add_cascade(label="파일(F)", menu=menu_file)
 
 # 편집 메뉴
@@ -16,20 +26,24 @@ menu_file = Menu(topmenu, tearoff=0)
 menu_file.add_command(label="   새로 만들기")
 topmenu.add_cascade(label="편집(E)", menu=menu_file)
 
-# 편집 메뉴
+# 포맷 메뉴
 menu_file = Menu(topmenu, tearoff=0)
 menu_file.add_command(label="   새로 만들기")
-topmenu.add_cascade(label="포맷(o)", menu=menu_file)
+topmenu.add_cascade(label="포맷(O)", menu=menu_file)
 
-# 편집 메뉴
+# 보기 메뉴
 menu_file = Menu(topmenu, tearoff=0)
 menu_file.add_command(label="   새로 만들기")
-topmenu.add_cascade(label="보기(v)", menu=menu_file)
+topmenu.add_cascade(label="보기(V)", menu=menu_file)
 
-# 편집 메뉴
+# 도움말 메뉴
 menu_file = Menu(topmenu, tearoff=0)
 menu_file.add_command(label="   새로 만들기")
 topmenu.add_cascade(label="도움말(H)", menu=menu_file)
+
+# 메모 적기
+textedit = Text(root)
+textedit.pack()
 
 root.config(menu=topmenu)
 root.mainloop()
