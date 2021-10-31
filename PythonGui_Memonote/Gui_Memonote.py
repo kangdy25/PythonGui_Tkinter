81d89b3a45bb4cb8302b00778b07b20449632633
Kangdy <canfrog@naver.com>
2021-10-31 22:43:27 +0900

    Make Gui and Topmenu

--- PythonGui_Memonote/Gui_Memonote.py
+++ PythonGui_Memonote/Gui_Memonote.py
@@ -4,4 +4,32 @@ root = Tk()
 root.title("제목 없음-메모장")
 root.geometry("640x480")
 
+topmenu = Menu(root)
+
+# 파일 메뉴
+menu_file = Menu(topmenu, tearoff=0)
+menu_file.add_command(label="   새로 만들기")
+topmenu.add_cascade(label="파일(F)", menu=menu_file)
+
+# 편집 메뉴
+menu_file = Menu(topmenu, tearoff=0)
+menu_file.add_command(label="   새로 만들기")
+topmenu.add_cascade(label="편집(E)", menu=menu_file)
+
+# 편집 메뉴
+menu_file = Menu(topmenu, tearoff=0)
+menu_file.add_command(label="   새로 만들기")
+topmenu.add_cascade(label="포맷(o)", menu=menu_file)
+
+# 편집 메뉴
+menu_file = Menu(topmenu, tearoff=0)
+menu_file.add_command(label="   새로 만들기")
+topmenu.add_cascade(label="보기(v)", menu=menu_file)
+
+# 편집 메뉴
+menu_file = Menu(topmenu, tearoff=0)
+menu_file.add_command(label="   새로 만들기")
+topmenu.add_cascade(label="도움말(H)", menu=menu_file)
+
+root.config(menu=topmenu)
 root.mainloop()
